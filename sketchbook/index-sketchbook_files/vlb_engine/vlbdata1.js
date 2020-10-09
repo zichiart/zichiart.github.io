6e0c03460fa514d1bbb3fb072801c0a1569ac0c9
zichi <zichi@mac.com>
2020-10-09 22:46:23 +0900

    New Files
    New files added

---
+++ sketchbook/index-sketchbook_files/vlb_engine/vlbdata1.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({autoPlay:true,borderSize:18,classNames:'vlightbox1',closeLocation:'nav',descSliding:true,enableRightClick:false,enableSlideshow:true,overlayOpacity:0.8,prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) });
